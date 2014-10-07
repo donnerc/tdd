b163ed1ada093c7a895835f02486220850a97bdb
Cédric Donner <cedonner@gmail.com>
2014-10-07 14:46:52 +0000

    home view now returns basic html

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from lists.views import home_page
+from django.http import HttpRequest
 
 # test pour tester que les tests fonctionnent
 # class SmokeTest(TestCase):
@@ -14,4 +15,11 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
         
-    
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        
+        # ce test ne dit pas que la balise title est enfermée dans la balise <head> ...'
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
