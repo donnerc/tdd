32b9bc51f1345505c7a8ff00b3222cab2e0c21f9
Cédric Donner <cedonner@gmail.com>
2014-10-13 06:14:18 +0000

    Model for list Items and associated migrations

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,8 @@ from django.template.loader import render_to_string
 from lists.views import home_page
 from django.http import HttpRequest
 
+from lists.models import Item
+
 # test pour tester que les tests fonctionnent
 # class SmokeTest(TestCase):
 #     def test_bad_maths(self):
@@ -35,4 +37,22 @@ class HomePageTest(TestCase):
             {'new_item_text' : 'new todo item'}
         )
         self.assertEqual(response.content.decode(), expected_html)
+        
+        
+class ItemModelTest(TestCase):
+    
+    def test_saving_and_retrieving_items(self):
+        item1 = Item()
+        item1.text = "Mon premier item dans la liste"
+        item1.save()
+        
+        item2 = Item()
+        item2.text = "Deuxième item"
+        item2.save()
+        
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        
+        self.assertEqual(saved_items[0].text, "Mon premier item dans la liste")
+        self.assertEqual(saved_items[1].text, "Deuxième item")
         
