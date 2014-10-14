8d469854bb29080538b19871d95e4e150d5cfe01
Cédric Donner <cedonner@gmail.com>
2014-10-14 07:37:40 +0200

    added functional tests for separate lists and list url generation

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
+
 class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
@@ -43,12 +44,12 @@ class NewVisitorTest(LiveServerTestCase):
         # il entre la première chose à faire le matin
         inputbox.send_keys('Rendre grâce au Créateur')
 
-        # lorsqu'elle presse sur la touche ENTER, la page se raffraichit et la
-        # page liste la tâche entrée
-        # "1: Rendre grâce au Créateur"
-        # en tant qu'élément d'une liste à puces
+        # lorsqu'elle presse sur la touche ENTER, une nouvelle URL est générée
+        # pour sa nouvelle liste contenant comme premier élément "1: Rendre
+        # grâce au Créateur"
         inputbox.send_keys(Keys.ENTER)
-
+        bill_list_url = self.browser.current_url
+        self.assertRegex(bill_list_url, '/lists/.+')
         self.check_for_row_in_list_table("1: Rendre grâce au Créateur")
 
         # il y a toujours le champ qui permet d'insérer des tâches. Bill va
@@ -62,6 +63,45 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table("1: Rendre grâce au Créateur")
         self.check_for_row_in_list_table("2: Se laver")
 
+        # Cunéconde, la copine de Bill est impressionnée de la manière dont
+        # Bill s'organise car il n'oublie plus de vider la poubelle. Elle
+        # décide donc de se créer également une todo list.
 
+        ## On utilise une nouvelle session pour s'assurer que
+        ## Cunégonde n'arrive pas sur la liste de Bill
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Cunégonde visite la page d'accueil et il n'y a aucune trace de la
+        # liste de Bill
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Rendre grâce au Créateur', page_text)
+        self.assertNotIn('Se laver', page_text)
+
+        # Cunégonde commence une nouvelle liste en créant une première tâche
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Prendre une douche')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Cunégonde reçoit une URL privée pour sa liste
+        cunegonde_list_url = self.browser.current_url
+        self.assertRegex('/lists/.+')
+        self.assertNotEqual(bill_list_url, cunegonde_list_url)
+
+        # il n'y a toujours aucune trace de la liste de Bill
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Rendre grâce au Créateur', page_text)
+        self.assertNotIn('Se laver', page_text)
+
+        # tous deux satisfaits, ils partent joyeusement au travail
+        self.assertTrue(False, 'terminer ce test')
+
+
+
+
+
+# ceci n'est plus utile puisque l'on utilise LiveServerTestCase et que le test
+# est chargé comme un module
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
