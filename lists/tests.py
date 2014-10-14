9eda71892e4fdb4e337428830b06c66b5418ea9a
Cédric Donner <cedonner@gmail.com>
2014-10-14 09:43:09 +0200

    added List model, updated tests to reflect this new state

--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,7 @@ from django.template.loader import render_to_string
 from lists.views import home_page
 from django.http import HttpRequest
 
-from lists.models import Item
+from lists.models import Item, List
 
 # test pour tester que les tests fonctionnent
 # class SmokeTest(TestCase):
@@ -44,29 +44,40 @@ class NewListTest(TestCase):
 
         self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         item1 = Item()
         item1.text = "Mon premier item dans la liste"
+        item1.list = list_
         item1.save()
 
         item2 = Item()
         item2.text = "Deuxième item"
+        item2.list = list_
         item2.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
 
         self.assertEqual(saved_items[0].text, "Mon premier item dans la liste")
+        self.assertEqual(saved_items[0].list, list_)
         self.assertEqual(saved_items[1].text, "Deuxième item")
+        self.assertEqual(saved_items[1].list, list_)
 
 
 class ListViewTest(TestCase):
 
     def test_display_all_items(self):
-        Item.objects.create(text='premier item')
-        Item.objects.create(text='deuxième item')
+        list_ = List.objects.create()
+        Item.objects.create(text='premier item', list=list_)
+        Item.objects.create(text='deuxième item', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
