d464f2b450a1664bd1e269e55d9835582b48ef18
Cédric Donner <cedonner@gmail.com>
2014-10-29 11:38:20 +0100

    updated FT to run against staging server

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -2,12 +2,28 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 
+import sys
+
 
 class FunctionalTest(StaticLiveServerTestCase):
 
     # TODO: lorsqu'on utilise un 'staging site', il faut adapter ces tests
     # pour pouvoir les exécuter sur ce serveur, notamment, il faudra utiliser
     # desméthodes de classe pour setUp et tearDown. Cf http://chimera.labs.oreilly.com/books/1234000000754/ch08.html#_as_always_start_with_a_test
+
+    @classmethod
+    def setUpClass(cls):  #1
+        for arg in sys.argv:  #2
+            if 'liveserver' in arg:  #3
+                cls.server_url = 'http://' + arg.split('=')[1]  #4
+                return  #5
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Firefox()
--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -6,23 +6,33 @@ from selenium.webdriver.common.keys import Keys
 
 class LayoutAndStylingTest(FunctionalTest):
 
-    @skip
     def test_cannot_add_empty_list(self):
 
         # Bill se rend su rl page d'accueil et tente d'insérer une tache vide
         # accidentellement. Elle tape Enter dans la liste d'entrée.
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
         # la page d'accueil se charge avec un message d'erreur indiquant qu'il
         # n'est pas possible de créer une nouvelle liste avec une tâche vide.
+        error = self.browser.find_element_by_css_selector('.has-error') #1
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # Il fait une deuxième tentative avec du texte cette-fois ci, ce qui
         # fonctionne comme prévu.
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+        self.check_for_row_in_list_table('1: Buy milk') #2
 
         # Il décide de réessayer d'envoyer un item vide et reçoit un message
         # d'erreur.
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         # il peut corriger son erreur en remplissant le champ approprié.
-        self.fail("Write Me !!!")
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('2: Make tea')
 
 # ceci n'est plus utile puisque l'on utilise LiveServerTestCase et que le test
 # est chargé comme un module
